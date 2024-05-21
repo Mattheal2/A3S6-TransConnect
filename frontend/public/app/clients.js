de6c015435681c8c1b09318ef8327ae5fe86e7ac
merlleu <r.langdorph@gmail.com>
2024-05-21 18:42:26 +0200

    update js for client

--- frontend/public/app/clients.js
+++ frontend/public/app/clients.js
@@ -147,9 +147,9 @@ $("#navbar-actions-list").append($(`
 <a href="#" class="btn btn-primary d-none d-sm-inline-block">
     <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 5l0 14"></path><path d="M5 12l14 0"></path></svg>
-    Nouvel employé
+    Nouveau client
 </a>
-<a href="#" class="btn btn-primary d-sm-none btn-icon" data-bs-toggle="modal"aria-label="Nouvel employé">
+<a href="#" class="btn btn-primary d-sm-none btn-icon" data-bs-toggle="modal"aria-label="Nouveau client">
     <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
     <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"></path><path d="M12 5l0 14"></path><path d="M5 12l14 0"></path></svg>
 </a>
